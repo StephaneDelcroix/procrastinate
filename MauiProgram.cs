118f8a98ba292c398e9dea42579ed66810623542
Stephane Delcroix <stephane@delcroix.org>
2025-12-15 20:14:41 +0100

    Replace emojis with FontAwesome icons
    - Added FontAwesome 6 Solid font
    - Updated all pages to use FA icons instead of emojis
    - Icons: clipboard-list, gamepad, theater-masks, chart-line, cog
    - Consistent icon styling with theme colors
    - Better cross-platform rendering

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -15,6 +15,7 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("FontAwesome-Solid.otf", "FontAwesomeSolid");
 			});
 
 		// Services
